46d494bb6273d92c2fea7b7748e6c74e9b8f2be3
HallGold <wangyimiao666@gmail.com>
2022-03-11 11:35:10 +0800

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-  printWidth: 120, // 每行代码长度（默认80）
+  printWidth: 150,
   tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
   semi: false,
   singleQuote: true,
-  jsxBracketSameLine: true, // 多行JSX中的>放置在最后一行的结尾，而不是另起一行（默认false）
-  jsxSingleQuote: true
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: true,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
 }
